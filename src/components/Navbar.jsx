0b3e7349a3f47e022ab379ba717bfe9752b400c9
Matias Coniglio <matiasconiglio1@gmail.com>
2026-04-15 13:53:41 -0300

    se hizo cambio de titulo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/">Home</Link> | <Link to="/events">Eventos</Link>
+      <Link to="/">Home</Link> | <Link to="/events">Eventossssss</Link>
     </nav>
   );
 }
